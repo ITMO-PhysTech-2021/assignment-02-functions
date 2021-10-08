e69fb5ba4663ce0c235162df0d8fb9da77310230
amathael <dan.io.oreshnikov@gmail.com>
2021-10-08 23:49:00 +0300

    Fixed all tests for 'rprint', removed 'calculator2'

--- practice/rprint/rprint.py
+++ practice/rprint/rprint.py
@@ -3,12 +3,12 @@ def flatten(a):
     pass
 
 
-def rprint(a, depth=1, current_depth=0):
+def rprint(a, depth, current_depth=0):
     """Вернуть строковое представление массива, обрезав его по глубине depth"""
     pass
 
 
 # noinspection PyShadowingBuiltins
-def pretty_rprint(a, print, current_depth=0):
+def pretty_rprint(a, current_depth=0, print=print):
     """Вывести массив, делая отступы, соответствующие уровню вложенности"""
     pass
